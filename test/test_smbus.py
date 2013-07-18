22b228bd73ce1b2440cdf666788d945913abdebd
David Schneider <david.schneider@bivab.de>
2013-07-18 16:59:41 +0200

    add some workarounds to test against the C library too

--- test/test_smbus.py
+++ test/test_smbus.py
@@ -1,40 +1,50 @@
 from smbus import SMBus
 import py
 
+
+class SMBusProxy(object):
+    def __init__(self, bus=-1):
+        self.called = False
+        self.passed_args = ()
+        self.smbus = SMBus() 
+        if bus != -1:
+            self.open(bus)
+
+    def open(self, *args):
+        self.called = True
+        self.passed_args = args
+
+    def __getattr__(self, name, *args, **kwargs):
+        return getattr(self.smbus, name, *args, **kwargs)
+
 class TestSMBus_init(object):
-    called = False
-    passed_args = None
-    #tmp = None
-
-    def setup_method(self, *args):
-        self.tmp = SMBus.open
-        def open_helper(*args):
-            self.called = True
-            self.passed_args = args
-        SMBus.open = open_helper
-
-    def teardown_method(self, *args):
-        SMBus.open  = self.tmp
 
     def test_init_with_arg_tries_to_open(self):
-        bus = SMBus(12)
+        bus = SMBusProxy(12)
 
-        assert self.called
-        assert self.passed_args == (bus, 12)
+        assert bus.called
+        assert bus.passed_args == (12,)
 
     def test_init_does_nothing_by_default(self):
-        bus = SMBus()
-
-        assert bus.fd == -1
-        assert bus.addr == -1
-        assert bus.pec == 0
-        assert not self.called
+        bus = SMBusProxy()
+        if hasattr(bus, '_fd'):
+            assert bus._fd == -1
+            assert bus._addr == -1
+            assert bus._pec == 0
+        assert not bus.called
 
 
 def test_open():
-    b = SMBus()
-    py.test.raises(IOError, 'b.open(-13)')
+    bus = SMBus()
+    py.test.raises(IOError, 'bus.open(-13)')
 
-    b.open(1)
-    assert b.fd != -1
+    bus.open(1)  # does not raise
 
+    if hasattr(bus, '_fd'):
+        assert b.fd != -1
+
+def test_write_quick(): 
+    bus = SMBus(1)
+    py.test.raises(TypeError, "bus.write_quick('a')")
+    py.test.raises(IOError, "bus.write_quick(4)")
+    bus.write_quick(1)
