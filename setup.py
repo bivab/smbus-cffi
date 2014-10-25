77ddf3f91d21fc3d8016766e766d2e1039481d17
David Schneider <david.schneider@bivab.de>
2014-10-25 10:52:40 -0600

    Move package information info an __about__ file. Based on the cryptography package

--- setup.py
+++ setup.py
@@ -6,24 +6,30 @@ import cffi
 import os
 import smbus
 
-readme = os.path.join(os.path.dirname(__file__), 'README.rst')
+base_dir = os.path.dirname(__file__)
+
+about = {}
+with open(os.path.join(base_dir, "smbus", "__about__.py")) as f:
+    exec(f.read(), about)
+
+readme = os.path.join(base_dir, 'README.rst')
 with open(readme) as f:
         long_description = f.read()
 
 setup(
-    name='smbus-cffi',
-    version='0.4',
-    description='This Python module allows SMBus access through the I2C /dev interface on Linux hosts. The host kernel must have I2C support, I2C device interface support, and a bus adapter driver.',
+    name=about['__title__'],
+    version=about['__version__'],
+    description=about['__summary__'],
     long_description=long_description,
-    author='David Schneider',
-    author_email='david.schneider@bivab.de',
-    url='https://github.com/bivab/smbus-cffi',
+    author=about['__author__'],
+    author_email=about['__email__'],
+    url=about['__uri__'],
+    license=about['__license__'],
     packages=find_packages(exclude=["test*", "*test*"]),
     zip_safe=False,
     ext_package='smbus',
     ext_modules=[smbus.ffi.verifier.get_extension()],
     install_requires=['cffi >= 0.8'],
-    license='GPLv2',
 
     classifiers=[
         'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
