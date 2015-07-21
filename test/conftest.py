b4ac66526b46aabfa24e689aa293d2f93dce9569
David Schneider <david.schneider@bivab.de>
2015-07-21 10:53:45 +0200

    Place generated module in smbus/ for testing

--- test/conftest.py
+++ test/conftest.py
@@ -1,3 +1,7 @@
+import os
+
+
 def pytest_configure(config):
     from smbus_cffi_build import ffi
-    ffi.compile()
+    target = os.path.join(os.path.dirname(__file__), '..', 'smbus')
+    ffi.compile(tmpdir=target)
